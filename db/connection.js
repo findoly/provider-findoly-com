0dc5e2b40bf13118e94126badca427657c53f5ba
Livcrew Dhiraj <admin@livcrew.com>
2026-07-11 18:52:12 +0530

    provider pushed

--- db/connection.js
+++ db/connection.js
@@ -7,7 +7,7 @@ async function connectDatabase() {
   }
 
   const uri =
-    process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/service_crm_admin";
+    process.env.MONGODB_URI;
 
   mongoose.set("strictQuery", true);
   await mongoose.connect(uri, {
